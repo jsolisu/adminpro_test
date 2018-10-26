23d04746fc7c6c168324d97db48b5b3822f51f67
jsolisucads <jsolis@cads.com.gt>
2018-10-26 11:09:51 -0600

    Agregar registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { PagesComponent } from './pages/pages.component';
 
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
@@ -20,7 +21,7 @@ const appRoutes: Routes = [
         ]
     },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', component: NopagefoundComponent }
 ];
 
