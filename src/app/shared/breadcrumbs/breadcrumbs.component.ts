1b70fb9a3e2b7bcb5685ffefe6887e320dfe4304
jsolisu <jcsolisu2000@gmail.com>
2018-11-11 11:32:29 -0600

    Titulos

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
 import { filter, map } from 'rxjs/operators';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -11,12 +12,13 @@ export class BreadcrumbsComponent implements OnInit {
 
   titulo: string;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private title: Title) {
 
     this.getDataRoute()
       .subscribe(data => {
         this.titulo = data.titulo;
-    });
+        this.title.setTitle(`AdminPro - ${this.titulo}`);
+      });
   }
 
   ngOnInit() {
